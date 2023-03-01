1421b2c2f5fa1bd2f8e746c45c82ed40440abd64
MichaelJayFox <125122867+MichaelJayFox@users.noreply.github.com>
2023-03-01 20:10:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,11 +9,11 @@ double pown(double value, uint16_t n) {
     value *= q;
   return q;
 }
-  
+
 uint64_t fact(uint16_t n) {
   uint64_t result = 1;
   for (uint64_t i = 1; i <= n; i++)
-    return *= i;
+    result *= i;
   return result;
 }
 double calcItem(double x, uint16_t n) {
@@ -30,15 +30,15 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double sinus = 0;
-    for (uint16_t i = 0; i <= count; i++)
-        sinus += pown(-1, n) * pown(x, 2*n+1) / fact(2*n+1)
-    return sinus;
+  for (uint16_t i = 0; i <= count; i++)
+      sinus += (pown(-1, i - 1) * (pown(x, 2 * i - 1) / (fact(2 * i - 1))));
+  return sinus;
 }
 
 double cosn(double x, uint16_t count) {
-    double otv = 1;
+    double cos = 1;
     for (int i = 2; i <= count; i++) {
-        otv += (pown(-1, i - 1) * (pown(x, 2 * i - 2) / (fact(2 * i - 2))));
+        cos += (pown(-1, i - 1) * (pown(x, 2 * i - 2) / (fact(2 * i - 2))));
     }
-    return otv;
+    return cos;
 }
