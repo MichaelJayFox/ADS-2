63270a280732010be57155c7a9f147d784d6ece9
MichaelJayFox <125122867+MichaelJayFox@users.noreply.github.com>
2023-03-01 21:07:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,21 +4,20 @@
 
 
 double pown(double value, uint16_t n) {
-  double q = value;
-  for (uint16_t i = 1; i < n; i++)
-    value *= q;
+  double q = 1;
+  for (uint16_t i = 1; i <= n; i++)
+    q *= value;
   return q;
 }
 
 uint64_t fact(uint16_t n) {
   uint64_t result = 1;
-  for (uint64_t i = 1; i <= n; i++)
+  for (uint64_t i = 2; i <= n; i++)
     result *= i;
   return result;
 }
 double calcItem(double x, uint16_t n) {
-  double result = pown(x, n) / fact(n);
-  return (pown(x,n) / fact(n));
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
@@ -30,15 +29,14 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double sinus = 0;
-  for (uint16_t i = 2; i <= count; i++)
-      sinus += (pown(-1, i - 1) * (pown(x, 2 * i - 1) / (fact(2 * i - 1))));
+  for (uint16_t i = 1; i <= count; i++)
+      sinus += pown(-1, i - 1) * (pown(x, 2 * i - 1) / fact(2 * i - 1);
   return sinus;
 }
 
 double cosn(double x, uint16_t count) {
-    double cos = 1;
-    for (int i = 2; i <= count; i++) {
-        cos += (pown(-1, i - 1) * (pown(x, 2 * i - 2) / (fact(2 * i - 2))));
-    }
+    double cos = 0;
+    for (int i = 1; i <= count; i++)
+        cos += pown(-1, i - 1) * pown(x, 2 * i - 2) / fact(2 * i - 2);
     return cos;
 }
